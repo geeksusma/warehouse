b4e057b6e80631e532fed7e88b013e2e38712a92
Jesus M <jesus.maria.villar.vazquez@apipana.io>
2026-03-16 10:31:24 +0100

    adding lombok and replacing ide getters and constructor by lombok annotations

--- src/main/java/es/geeksusma/warehouse/api/dto/ItemDTO.java
+++ src/main/java/es/geeksusma/warehouse/api/dto/ItemDTO.java
@@ -1,5 +1,10 @@
 package es.geeksusma.warehouse.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ItemDTO {
     private Long id;
     private String serialNumber;
@@ -7,34 +12,5 @@ public class ItemDTO {
     private String description;
     private Integer stock;
 
-    public ItemDTO() {
-    }
 
-    public ItemDTO(Long id, String serialNumber, String name, String description, Integer stock) {
-        this.id = id;
-        this.serialNumber = serialNumber;
-        this.name = name;
-        this.description = description;
-        this.stock = stock;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
 }
